a51c93ebb93e9921406a184590364efa023230e3
irshadxelpmoc <irshad@xelpmoc.in>
2017-12-11 16:42:21 +0530

    images

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
 
 import React, { Component } from "react";
 import { Platform, StyleSheet, Text, View } from "react-native";
-import HomePage from "./src/HomePage";
+import ImageShow from "./src/ImageShow";
 
 const instructions = Platform.select({
   ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
@@ -17,7 +17,7 @@ const instructions = Platform.select({
 
 export default class App extends Component {
   render() {
-    return <HomePage />;
+    return <ImageShow />;
   }
 }
 
